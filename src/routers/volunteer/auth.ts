3ea5bdd4ba7b3fed018d2e2d22192fa132dfdccd
RalphSaridar369 <ralphsaridar@hotmail.com>
2024-08-20 18:00:28 +0300

    Added dtos

--- src/routers/volunteer/auth.ts
+++ src/routers/volunteer/auth.ts
@@ -1,8 +1,9 @@
-import { login } from "../../controllers/volunteer/auth";
+import { login, register } from "../../controllers/volunteer/auth";
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/auth", login);
+router.post("/auth", login);
+router.post("/auth/register", register);
 
 export default router;
